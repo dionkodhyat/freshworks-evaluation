20028f438ff67ce5f025ac62cffd9923d908be40
Dion Kodhyat <dionkodhyat@pm.me>
2021-04-23 09:01:15 -0700

    Seeded db and created method to view parks from db

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express()
+const db = require('./queries')
 
-app.get('/', (req, res) => {
-    res.sendStatus(200);
-})
+app.use(cors())
+app.use(express.json())
+app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  )
+
+
+
+app.get('/park', db.getPark)
 
 app.listen(9001, () => {
     console.log("LISTENING ON PORT 9001")
--- queries.js
+++ queries.js
@@ -1,2 +1,14 @@
 const pool = require("./db")
 const SQL = require('sql-template-strings')
+
+const getPark = (req ,res) => {
+    pool.query("SELECT * FROM park", (err, response) => {
+        if (err) throw err
+        res.status(200).json(response.rows)
+    })
+}
+
+
+module.exports = {
+    getPark
+}
